d661c82e364d51357e5c90516d5917a9d7798567
YuliyaKalashnikova <pmalian@list.ru>
2023-01-25 22:20:09 +0700

    install prettier and eslint

--- src/app.js
+++ src/app.js
@@ -1,3 +1,3 @@
 export function App() {
-    return <h1 className="text-3xl bg-pink-200 font-bold underline">Hello world!</h1>;
-  }
+  return <h1 className='bg-pink-200 text-3xl font-bold underline'>Hello world!</h1>
+}
