61fa59c2c703b2aaeb1ede7b71529334828ed7e5
YuliyaKalashnikova <pmalian@list.ru>
2023-01-27 23:26:43 +0700

    refactor(components): create Empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Container from './components/Container'
 import Header from './components/Header'
 import Form from './components/Form'
+import Empty from './components/Empty'
 
 export const App = () => {
   return (
@@ -8,8 +9,9 @@ export const App = () => {
       <Header>
         <Form></Form>
       </Header>
+      <Empty />
 
-      <div className='space-y-4 p-6 text-center text-slate-400'>Задач нет</div>
+      {/* <div className='space-y-4 p-6 text-center text-slate-400'>Задач нет</div> */}
     </Container>
   )
 }
---
+++ src/components/Empty.jsx
@@ -0,0 +1,10 @@
+const Empty = (props) => {
+  return (
+    <div className='p-6 text-center text-slate-400'>
+      Задач нет
+      {props.children}
+    </div>
+  )
+}
+
+export default Empty
