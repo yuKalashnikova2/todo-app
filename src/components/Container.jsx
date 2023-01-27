eac6eeeb65fceeb7b3d116b19f71cd8545be05ff
YuliyaKalashnikova <pmalian@list.ru>
2023-01-27 21:05:56 +0700

    refactor(components): change width  in Container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 const Container = (props) => {
   return (
-    <div className='container mx-auto mt-48 w-2/4 overflow-hidden rounded-2xl shadow-md shadow-black/40 ring-black/40'>
+    <div className='container mx-auto my-48 max-w-xl overflow-hidden rounded-2xl shadow-md shadow-black/40 ring-black/40'>
       {props.children}
     </div>
   )
