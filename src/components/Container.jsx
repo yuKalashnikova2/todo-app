6085144886660219055e90ede1fd14b52d994419
YuliyaKalashnikova <pmalian@list.ru>
2023-01-28 23:14:20 +0700

    style(components): update styles Container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,5 @@
 const Container = (props) => {
-  return (
-    <div className='container absolute top-1/3 left-1/4 max-w-xl -translate-y-2/4 overflow-hidden rounded-2xl shadow-md shadow-black/40 ring-black/40'>
-      {props.children}
-    </div>
-  )
+  return <div className='container max-w-xl overflow-hidden rounded-lg shadow-md'>{props.children}</div>
 }
 
 export default Container
