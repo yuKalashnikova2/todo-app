268e3c1184a3e5d019afdf3e81dca4184c6e9de9
YuliyaKalashnikova <pmalian@list.ru>
2023-01-31 18:41:07 +0700

    feat(components): counter
    create Counter

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Form from './components/Form'
 import Empty from './components/Empty'
 import Task from './components/Task'
+import Counter from './components/Counter'
 
 export const App = () => {
   const [taskList, setTaskList] = useState([])
@@ -42,6 +43,8 @@ export const App = () => {
                 ))}
           </div>
         </Empty>
+
+        <Counter countCompleted={completedTaskList.length} countTotal={taskList.length}></Counter>
       </Container>
     </Page>
   )
---
+++ src/components/Counter.jsx
@@ -0,0 +1,9 @@
+const Counter = ({ countCompleted, countTotal }) => {
+  return (
+    <div>
+      Выполнено {countCompleted} из {countTotal}
+    </div>
+  )
+}
+
+export default Counter
