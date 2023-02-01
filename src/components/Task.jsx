1b6b4b0a7195e7530c40a5555a1f8d1b46c85c53
YuliyaKalashnikova <pmalian@list.ru>
2023-02-01 19:02:40 +0700

    style(components): styles checkbox Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,12 @@ const Task = ({ index, children, completed, onChange }) => {
   return (
     <div className='my-2 h-12 rounded-md bg-white px-6 py-2.5 text-start ring-2 ring-gray-200'>
       <label className={`task ${completed ? 'text-black' : 'text-gray-200'}`}>
-        <input type='checkbox' className='mr-4' checked={completed} onChange={() => onChange(children)} />
+        <input
+          type='checkbox'
+          className='mr-4 rounded text-blue-500 focus:ring-0'
+          checked={completed}
+          onChange={() => onChange(children)}
+        />
         {index}. {children}
       </label>
     </div>
