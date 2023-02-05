b7c5009e61443062baac98469c22e64f290d9d11
YuliyaKalashnikova <pmalian@list.ru>
2023-02-05 11:34:53 +0700

    feat(components): app: handle duplicate task, handle empty task error

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,46 @@ import Counter from './components/Counter'
 export const App = () => {
   const [taskList, setTaskList] = useState([])
   const [completedTaskList, setcompletedTaskList] = useState('')
+  const [error, setError] = useState('')
 
   const handleCompleteTask = (task) => {
     if (completedTaskList.includes(task)) {
-      setcompletedTaskList([...completedTaskList.filter((t) => t !== task)])
+      setcompletedTaskList(completedTaskList.filter((t) => t !== task))
     } else {
       setcompletedTaskList([...completedTaskList, task])
     }
+  }
+
+  const onSubmit = (task) => {
+    setError('')
+
+    if (!task) {
+      return
+    }
+
+    if (taskList.includes(task)) {
+      setError(<div className='uppercase text-red-500'>Такая задача уже есть!</div>)
+      return
+    }
+
+    setTaskList([...taskList, task])
   }
 
   return (
     <Page>
       <Container>
         <Header>
-          <Form onSubmit={(task) => setTaskList([...taskList, task])}></Form>
+          <Form onSubmit={onSubmit} />
         </Header>
         <Empty>
+          {error && (
+            <div className='flex justify-between'>
+              {error}{' '}
+              <button className='h-7 w-7 rounded-md bg-red-500 text-xl text-white' onClick={() => setError('')}>
+                x
+              </button>
+            </div>
+          )}
           <div className='my-2 flex flex-col'>
             {' '}
             {taskList.length === 0
