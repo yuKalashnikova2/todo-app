d1b4850fb6eee9f8ed8cb886ff0be7b1ec39f3ef
YuliyaKalashnikova <pmalian@list.ru>
2023-02-18 20:43:53 +0700

    refactor: reducers switch/case

--- src/components/TaskList/reducers.jsx
+++ src/components/TaskList/reducers.jsx
@@ -1,29 +1,35 @@
 export function taskReducer(taskList, action) {
-  if (action.type === 'ADD_TASK') {
-    const newTaskItem = {
-      id: taskList.length + 1,
-      name: action.task,
-      completed: false
-    }
-    return [...taskList, newTaskItem]
-  } else if (action.type === 'REMOVE_TASK') {
-    return taskList.filter((task) => task.id !== action.id)
-  } else if (action.type === 'COMPLETE_TASK') {
-    const newTaskList = taskList.map((task) => {
-      if (task.id === action.id) {
-        task.completed = action.value
+  switch (action.type) {
+    case 'ADD_TASK': {
+      const newTaskItem = {
+        id: taskList.length + 1,
+        name: action.task,
+        completed: false
       }
-      return task
-    })
-
-    return newTaskList
+      return [...taskList, newTaskItem]
+    }
+    case 'REMOVE_TASK': {
+      return taskList.filter((task) => task.id !== action.id)
+    }
+    case 'COMPLETE_TASK': {
+      const newTaskList = taskList.map((task) => {
+        if (task.id === action.id) {
+          task.completed = action.value
+        }
+        return task
+      })
+      return newTaskList
+    }
   }
 }
 
 export function errorReducer(error, action) {
-  if (action.type === 'SHOW_ERROR') {
-    return action.text
-  } else if (action.type === 'CLEAN_ERROR') {
-    return ''
+  switch (action.type) {
+    case 'SHOW_ERROR': {
+      return action.text
+    }
+    case 'CLEAN_ERROR': {
+      return ''
+    }
   }
 }
